50ebd8a31ee1aa933ae9664bdb426b09d8b12358
pin4495 <ezequiel_ramela@hotmail.com>
2023-07-07 14:49:21 -0300

    se actulizó el proyecto

--- Ejercicio_4/cuarto_ejercicio.py
+++ Ejercicio_4/cuarto_ejercicio.py
@@ -1,17 +1,3 @@
-a = 4
-b = 6
-# True and True
-#      True
-'''
-if a >= 5 and b <= 6 :
-    print('A es mayor o igual que 5 y B es menor o igual que 6')
-elif a >= 6:
-    print('a es mayor o igual que 6')
-else:
-    print('No se cumple NADA de lo anterior')
-print('Fin del IF')
-'''
-
 contador = 1
 while contador <= 100:
     print('contador vale:', contador)
